404c246ca672aa0df0f35586d6e69bc1e1d27290
Z Jav <56839787+ZJav1310@users.noreply.github.com>
2022-12-31 20:17:39 +0000

    began work on api

---
+++ src/main/java/com/enterpriseproject/enterpriseapi/FilmAPIServlet.java
@@ -0,0 +1,130 @@
+package com.enterpriseproject.enterpriseapi;
+
+import Models.Film;
+import jakarta.servlet.*;
+import jakarta.servlet.http.*;
+import jakarta.servlet.annotation.*;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.ArrayList;
+
+import Controllers.Commands;
+
+import javax.xml.parsers.ParserConfigurationException;
+
+@WebServlet(name = "FilmAPIServlet", value = "/FilmAPIServlet")
+public class FilmAPIServlet extends HttpServlet {
+    Commands commands;
+
+    {
+        try {
+            commands = new Commands();
+        } catch (ParserConfigurationException | SQLException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        response.setContentType("text/html;charset=UTF-8");
+        var writer = response.getWriter();
+
+        if(request.getParameterMap().containsKey("id")){
+            var film = commands.getById(Integer.parseInt(request.getParameter("id")));
+            System.out.println(film);
+            writer.println("GET ID.");
+        } else if (request.getParameterMap().containsKey("title")) {
+            var films = commands.searchByTitle(request.getParameter("title"));
+            for (Film f:films) {
+                System.out.println(f);
+            }
+            writer.println("GET TITLES.");
+        } else {
+            var films = (ArrayList<Film>) commands.getAllFilms();
+            for (Film f:films) {
+                System.out.println(f);
+            }
+            writer.println("GET ALL.");
+        }
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var writer = response.getWriter();
+        String message = "Failed";
+
+        if(request.getParameterMap().containsKey("title")
+                && request.getParameterMap().containsKey("year")
+                && request.getParameterMap().containsKey("stars")
+                && request.getParameterMap().containsKey("director")
+                && request.getParameterMap().containsKey("review"))
+        {
+            String title = request.getParameter("title");
+            String director = request.getParameter("director");
+            String stars = request.getParameter("stars");
+            String review = request.getParameter("review");
+            int year = Integer.parseInt(request.getParameter("year"));
+
+            Film f = new Film(title, year, director, stars, review);
+
+            int result = commands.insertFilm(f);
+
+            if( result > 0 ) {
+                message = "Inserted.";
+            }
+        }
+
+        writer.println(message);
+
+    }
+
+    @Override
+    protected void doDelete(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var writer = response.getWriter();
+        String message = "Failed";
+
+        if(request.getParameterMap().containsKey("id")){
+            int result = commands.deleteFilm(Integer.parseInt(request.getParameter("id")));
+
+            if(result>0){
+                message = "Deleted.";
+            }
+        }
+
+        writer.println(message);
+    }
+
+    @Override
+    protected void doPut(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        var writer = response.getWriter();
+        String message = "Failed";
+
+        if(request.getParameterMap().containsKey("title")
+                && request.getParameterMap().containsKey("year")
+                && request.getParameterMap().containsKey("stars")
+                && request.getParameterMap().containsKey("director")
+                && request.getParameterMap().containsKey("review")
+                && request.getParameterMap().containsKey("id"))
+        {
+            String title = request.getParameter("title");
+            String director = request.getParameter("director");
+            String stars = request.getParameter("stars");
+            String review = request.getParameter("review");
+            int year = Integer.parseInt(request.getParameter("year"));
+            int id = Integer.parseInt(request.getParameter("id"));
+
+
+            Film f = new Film(id, title, year, director, stars, review);
+
+            int result = commands.updateFilm(f);
+
+            if( result > 0 ) {
+                message = "Updated.";
+            }
+        }
+
+        writer.println(message);
+    }
+
+}
